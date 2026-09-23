cafee6a317de48cf4b56d3d0014eba3847580430
e-proo <tem.studyer@gmail.com>
2026-09-24 00:47:49 +0300

    refactor: remove superseded generic event exports

--- src/lib/services/platform/index.ts
+++ src/lib/services/platform/index.ts
@@ -1,6 +1,4 @@
 export * from './business-event-delivery'
-export * from './generic-message-projectors'
-export * from './generic-business-events'
 export * from './business-event-cutover'
 export * from './event-projector-registry'
 export * from './business-event-message-renderer'
