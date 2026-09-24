f7fc2f531c74c2f72926a8642cbd6fb2c71e9a81
e-proo <tem.studyer@gmail.com>
2026-09-24 04:17:41 +0300

    refactor: remove obsolete domain legacy notification adapter

--- src/lib/services/platform/legacy-notification-composition.ts
+++
@@ -1,8 +0,0 @@
-import { INTENTS_LEGACY_NOTIFICATION_RENDERER } from '@/lib/services/intents/legacy-notification-adapter'
-import { FX_V2_LEGACY_NOTIFICATION_RENDERER } from '@/lib/services/fx-v2/legacy-notification-adapter'
-import { LegacyCustomerNotificationRendererRegistry } from './legacy-notification-renderer-registry'
-
-export const CURRENT_LEGACY_NOTIFICATION_RENDERERS =
-  new LegacyCustomerNotificationRendererRegistry()
-    .register(FX_V2_LEGACY_NOTIFICATION_RENDERER)
-    .register(INTENTS_LEGACY_NOTIFICATION_RENDERER)
